3ecac7db8b07383356bbb6270229a2c888f91263
Max Asnaashari <max.asnaashari@canonical.com>
2022-06-29 05:08:21 +0000

    internal/db/cluster/schema.mapper: Simplify schema entry in cluster package
    We're not using the generated methods for the schema yet, so this
    removes the invoking of lxd-generate that was in place.
    
    Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>

--- internal/db/cluster/schema.go
+++ internal/db/cluster/schema.go
@@ -1,66 +1,12 @@
 package cluster
 
-import "time"
+import (
+	"time"
+)
 
-const SchemaVersion int = 0
-
-// CreateSchema is the default schema applied when bootstrapping the database.
-const CreateSchema = `
-CREATE TABLE internal_schemas (
-  id          INTEGER    PRIMARY  KEY    AUTOINCREMENT  NOT  NULL,
-  version     INTEGER    NOT      NULL,
-  updated_at  DATETIME   NOT      NULL,
-  UNIQUE      (version)
-);
-
-CREATE TABLE internal_certificates (
-  id           INTEGER        PRIMARY  KEY    AUTOINCREMENT  NOT  NULL,
-  fingerprint  TEXT           NOT      NULL,
-  type         INTEGER        NOT      NULL,
-  name         TEXT           NOT      NULL,
-  certificate  text           NOT      NULL,
-  UNIQUE       (fingerprint)
-);
-
-CREATE TABLE internal_token_records (
-  id           INTEGER         PRIMARY  KEY    AUTOINCREMENT  NOT  NULL,
-  joiner_cert  TEXT            NOT      NULL,
-  token        TEXT            NOT      NULL,
-  UNIQUE       (joiner_cert),
-  UNIQUE       (token)
-);
-
-CREATE TABLE internal_cluster_members (
-  id                   INTEGER   PRIMARY  KEY    AUTOINCREMENT  NOT  NULL,
-  name                 TEXT      NOT      NULL,
-  address              TEXT      NOT      NULL,
-  certificate          TEXT      NOT      NULL,
-  schema               INTEGER   NOT      NULL,
-  heartbeat            DATETIME  NOT      NULL,
-  role                 TEXT      NOT      NULL,
-  UNIQUE(name),
-  UNIQUE(address),
-  UNIQUE(certificate)
-);
-`
-
-//go:generate -command mapper lxd-generate db mapper -t schema.mapper.go
-//go:generate mapper reset
-//
-//go:generate mapper stmt -e internal_schema objects table=internal_schemas version=2
-//
-//go:generate mapper method -e internal_schema GetMany version=2
-
-// InternalSchema represents the database schema table.
-type InternalSchema struct {
+// Schema represents the database schema table.
+type Schema struct {
 	ID        int
-	Version   string `db:"primary=yes"`
+	Version   int `db:"primary=yes"`
 	UpdatedAt time.Time
-}
-
-// InternalSchemaFilter represents the database schema table.
-type InternalSchemaFilter struct {
-	ID        *int
-	Version   *string
-	UpdatedAt *time.Time
 }

a3573658394d659d224f00cec6c1cf60e9e66495
Max Asnaashari <max.asnaashari@canonical.com>
2022-06-29 05:04:26 +0000

    internal/db/cluster/cluster/member: Add schema version db helpers
    Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>

--- internal/db/cluster/cluster_member.go
+++ internal/db/cluster/cluster_member.go
@@ -1,10 +1,12 @@
 package cluster
 
 import (
+	"database/sql"
 	"fmt"
 	"time"
 
 	"github.com/canonical/microcluster/internal/rest/types"
+	"github.com/lxc/lxd/lxd/db/query"
 )
 
 //go:generate -command mapper lxd-generate db mapper -t cluster_members.mapper.go
@@ -71,4 +73,36 @@ func (c InternalClusterMember) ToAPI() (*types.ClusterMember, error) {
 		LastHeartbeat: c.Heartbeat,
 		Status:        types.MemberUnreachable,
 	}, nil
+}
+
+// UpdateClusterMemberSchemaVersion sets the schema version for the cluster member with the given address.
+// This helper is non-generated to work before generated statements are loaded, as we update the schema.
+func UpdateClusterMemberSchemaVersion(tx *sql.Tx, version int, address string) error {
+	stmt := "UPDATE internal_cluster_members SET schema=? WHERE address=?"
+	result, err := tx.Exec(stmt, version, address)
+	if err != nil {
+		return err
+	}
+
+	n, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if n != 1 {
+		return fmt.Errorf("updated %d rows instead of 1", n)
+	}
+
+	return nil
+}
+
+// GetClusterMemberSchemaVersions returns the schema versions from all cluster members that are not pending.
+// This helper is non-generated to work before generated statements are loaded, as we update the schema.
+func GetClusterMemberSchemaVersions(tx *sql.Tx) ([]int, error) {
+	sql := "SELECT schema FROM internal_cluster_members WHERE NOT role='pending'"
+	versions, err := query.SelectIntegers(tx, sql)
+	if err != nil {
+		return nil, err
+	}
+
+	return versions, nil
 }

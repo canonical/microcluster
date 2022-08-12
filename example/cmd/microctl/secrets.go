6c0037c0362299db762b3f060e5b2149d18dfec1
Max Asnaashari <max.asnaashari@canonical.com>
2022-08-12 03:07:08 +0000

    example/cmd/microctl/secrets: Implement tokens add/list/revoke
    Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>

--- example/cmd/microctl/secrets.go
+++ example/cmd/microctl/secrets.go
@@ -3,8 +3,10 @@ package main
 import (
 	"context"
 	"fmt"
+	"sort"
 
 	"github.com/canonical/microcluster/microcluster"
+	"github.com/lxc/lxd/lxc/utils"
 	"github.com/spf13/cobra"
 )
 
@@ -14,15 +16,42 @@ type cmdSecrets struct {
 
 func (c *cmdSecrets) Command() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "secrets <server.crt>",
+		Use:   "tokens",
 		Short: "Manage join tokens for MicroCluster",
+		RunE:  c.Run,
+	}
+
+	var cmdAdd = cmdTokensAdd{common: c.common}
+	cmd.AddCommand(cmdAdd.Command())
+
+	var cmdList = cmdTokensList{common: c.common}
+	cmd.AddCommand(cmdList.Command())
+
+	var cmdRevoke = cmdTokensRevoke{common: c.common}
+	cmd.AddCommand(cmdRevoke.Command())
+
+	return cmd
+}
+
+func (c *cmdSecrets) Run(cmd *cobra.Command, args []string) error {
+	return cmd.Help()
+}
+
+type cmdTokensAdd struct {
+	common *CmdControl
+}
+
+func (c *cmdTokensAdd) Command() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "add <name>",
+		Short: "Add a new join token under the given name",
 		RunE:  c.Run,
 	}
 
 	return cmd
 }
 
-func (c *cmdSecrets) Run(cmd *cobra.Command, args []string) error {
+func (c *cmdTokensAdd) Run(cmd *cobra.Command, args []string) error {
 	if len(args) != 1 {
 		return cmd.Help()
 	}
@@ -38,6 +67,78 @@ func (c *cmdSecrets) Run(cmd *cobra.Command, args []string) error {
 	}
 
 	fmt.Println(token)
+
+	return nil
+}
+
+type cmdTokensList struct {
+	common *CmdControl
+}
+
+func (c *cmdTokensList) Command() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "list",
+		Short: "List join tokens available for use",
+		RunE:  c.Run,
+	}
+
+	return cmd
+}
+
+func (c *cmdTokensList) Run(cmd *cobra.Command, args []string) error {
+	if len(args) != 0 {
+		return cmd.Help()
+	}
+
+	m, err := microcluster.App(context.Background(), c.common.FlagStateDir, c.common.FlagLogVerbose, c.common.FlagLogDebug)
+	if err != nil {
+		return err
+	}
+
+	records, err := m.ListJoinTokens()
+	if err != nil {
+		return err
+	}
+
+	data := make([][]string, len(records))
+	for i, record := range records {
+		data[i] = []string{record.Name, record.Token}
+	}
+
+	header := []string{"NAME", "TOKENS"}
+	sort.Sort(utils.ByName(data))
+
+	return utils.RenderTable(utils.TableFormatTable, header, data, records)
+}
+
+type cmdTokensRevoke struct {
+	common *CmdControl
+}
+
+func (c *cmdTokensRevoke) Command() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "revoke <name>",
+		Short: "Revoke the join token with the given name",
+		RunE:  c.Run,
+	}
+
+	return cmd
+}
+
+func (c *cmdTokensRevoke) Run(cmd *cobra.Command, args []string) error {
+	if len(args) != 1 {
+		return cmd.Help()
+	}
+
+	m, err := microcluster.App(context.Background(), c.common.FlagStateDir, c.common.FlagLogVerbose, c.common.FlagLogDebug)
+	if err != nil {
+		return err
+	}
+
+	err = m.RevokeJoinToken(args[0])
+	if err != nil {
+		return err
+	}
 
 	return nil
 }

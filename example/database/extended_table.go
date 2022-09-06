ad982d605813130ea64cb5a4749908e8f745d631
Max Asnaashari <max.asnaashari@canonical.com>
2022-09-06 21:04:00 +0000

    example/database/extended/table: Remove version=2 flag from lxd-generate comments
    Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>

--- example/database/extended_table.go
+++ example/database/extended_table.go
@@ -3,20 +3,20 @@ package database
 //go:generate -command mapper lxd-generate db mapper -t extended.mapper.go
 //go:generate mapper reset
 //
-//go:generate mapper stmt -d cluster -e extended_table objects table=extended_table version=2
-//go:generate mapper stmt -d cluster -e extended_table objects-by-Key table=extended_table version=2
-//go:generate mapper stmt -d cluster -e extended_table id table=extended_table version=2
-//go:generate mapper stmt -d cluster -e extended_table create table=extended_table version=2
-//go:generate mapper stmt -d cluster -e extended_table delete-by-Key table=extended_table version=2
-//go:generate mapper stmt -d cluster -e extended_table update table=extended_table version=2
+//go:generate mapper stmt -d cluster -e extended_table objects table=extended_table
+//go:generate mapper stmt -d cluster -e extended_table objects-by-Key table=extended_table
+//go:generate mapper stmt -d cluster -e extended_table id table=extended_table
+//go:generate mapper stmt -d cluster -e extended_table create table=extended_table
+//go:generate mapper stmt -d cluster -e extended_table delete-by-Key table=extended_table
+//go:generate mapper stmt -d cluster -e extended_table update table=extended_table
 //
-//go:generate mapper method -i -d cluster -e extended_table GetMany version=2
-//go:generate mapper method -i -d cluster -e extended_table GetOne version=2
-//go:generate mapper method -i -d cluster -e extended_table ID version=2
-//go:generate mapper method -i -d cluster -e extended_table Exists version=2
-//go:generate mapper method -i -d cluster -e extended_table Create version=2
-//go:generate mapper method -i -d cluster -e extended_table DeleteOne-by-Key version=2
-//go:generate mapper method -i -d cluster -e extended_table Update version=2
+//go:generate mapper method -i -d cluster -e extended_table GetMany
+//go:generate mapper method -i -d cluster -e extended_table GetOne
+//go:generate mapper method -i -d cluster -e extended_table ID
+//go:generate mapper method -i -d cluster -e extended_table Exists
+//go:generate mapper method -i -d cluster -e extended_table Create
+//go:generate mapper method -i -d cluster -e extended_table DeleteOne-by-Key
+//go:generate mapper method -i -d cluster -e extended_table Update
 
 // ExtendedTable is an example of a database table. In this case named `extended_table`. The above comments will
 // generate database queries and helpers using lxd-generate.

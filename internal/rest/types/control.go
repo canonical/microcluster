5a7db50b43d70c8006999118291befe3bfbc1d42
Max Asnaashari <max.asnaashari@canonical.com>
2022-09-20 19:05:41 +0000

    internal/rest/types/control: Add address and name to Control struct
    Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>

--- internal/rest/types/control.go
+++ internal/rest/types/control.go
@@ -1,7 +1,13 @@
 package types
 
+import (
+	"github.com/canonical/microcluster/rest/types"
+)
+
 // Control represents the arguments that can be used to initialize/shutdown the daemon.
 type Control struct {
-	Bootstrap bool   `json:"bootstrap" yaml:"bootstrap"`
-	JoinToken string `json:"join_token" yaml:"join_token"`
+	Bootstrap bool           `json:"bootstrap" yaml:"bootstrap"`
+	JoinToken string         `json:"join_token" yaml:"join_token"`
+	Address   types.AddrPort `json:"address" yaml:"address"`
+	Name      string         `json:"name" yaml:"name"`
 }

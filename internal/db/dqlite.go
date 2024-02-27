9d0477121314463219278c0d4836c5b2ba7003cd
Gabriel Mougard <gabriel.mougard@canonical.com>
2024-02-27 15:31:16 +0100

    microcluster/internal/db: Store initial API extensions in the bootstraped internal cluster member
    We want to store the API extensions for the initial internal cluster member.
    
    Signed-off-by: Gabriel Mougard <gabriel.mougard@canonical.com>

--- internal/db/dqlite.go
+++ internal/db/dqlite.go
@@ -109,6 +109,8 @@ func (db *DB) Bootstrap(extensions extensions.Extensions, project string, addr a
 		return err
 	}
 
+	// Apply initial API extensions on the bootstrap node.
+	clusterRecord.APIExtensions = extensions
 	err = db.Transaction(db.ctx, func(ctx context.Context, tx *sql.Tx) error {
 
 		_, err := cluster.CreateInternalClusterMember(ctx, tx, clusterRecord)

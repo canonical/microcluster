fc6b021f2bf5ac74a3c4536bb3e170802afecb23
Angelos Kolaitis <angelos.kolaitis@canonical.com>
2024-04-10 12:41:10 +0300

    trust: expose trust.Location type
    Signed-off-by: Angelos Kolaitis <angelos.kolaitis@canonical.com>

---
+++ trust/trust.go
@@ -0,0 +1,6 @@
+package trust
+
+import "github.com/canonical/microcluster/internal/trust"
+
+// Location exposes the internal trust location type for use with extended API handlers.
+type Location = trust.Location

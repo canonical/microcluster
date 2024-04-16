bb6db04a0b9acc5bb4003a4db18440dbfcec8cc4
Angelos Kolaitis <angelos.kolaitis@canonical.com>
2024-04-16 15:06:23 +0300

    internal/rest/client: drop unused timeout flag from ControlDaemon
    Signed-off-by: Angelos Kolaitis <angelos.kolaitis@canonical.com>

--- internal/rest/client/control.go
+++ internal/rest/client/control.go
@@ -2,19 +2,11 @@ package client
 
 import (
 	"context"
-	"time"
 
 	"github.com/canonical/microcluster/internal/rest/types"
 )
 
 // ControlDaemon posts control data to the daemon.
-func (c *Client) ControlDaemon(ctx context.Context, args types.Control, timeout time.Duration) error {
-	if timeout == 0 {
-		return c.QueryStruct(ctx, "POST", ControlEndpoint, nil, args, nil)
-	}
-
-	queryCtx, cancel := context.WithTimeout(ctx, timeout)
-	defer cancel()
-
-	return c.QueryStruct(queryCtx, "POST", ControlEndpoint, nil, args, nil)
+func (c *Client) ControlDaemon(ctx context.Context, args types.Control) error {
+	return c.QueryStruct(ctx, "POST", ControlEndpoint, nil, args, nil)
 }

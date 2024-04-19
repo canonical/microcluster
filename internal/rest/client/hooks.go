da2eff8e6fccb684311953b2a5efec40e3595820
Max Asnaashari <max.asnaashari@canonical.com>
2024-04-19 23:54:09 +0000

    internal/rest/resources: Add /internal/hooks endpoint
    Adds POST method that can be used to execute various hooks.
    
    For now, this is limited to just PreRemove, PostRemove and
    OnNewMember, which are run across the whole cluster.
    
    Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>

---
+++ internal/rest/client/hooks.go
@@ -0,0 +1,34 @@
+package client
+
+import (
+	"context"
+	"time"
+
+	"github.com/canonical/lxd/shared/api"
+
+	"github.com/canonical/microcluster/internal/rest/types"
+)
+
+// RunPreRemoveHook executes the PreRemove hook with the given configuration on the cluster member targetted by this client.
+func RunPreRemoveHook(ctx context.Context, c *Client, config types.HookRemoveMemberOptions) error {
+	queryCtx, cancel := context.WithTimeout(ctx, 30*time.Second)
+	defer cancel()
+
+	return c.QueryStruct(queryCtx, "POST", InternalEndpoint, api.NewURL().Path("hooks", string(types.PreRemove)), config, nil)
+}
+
+// RunPostRemoveHook executes the PostRemove hook with the given configuration on the cluster member targetted by this client.
+func RunPostRemoveHook(ctx context.Context, c *Client, config types.HookRemoveMemberOptions) error {
+	queryCtx, cancel := context.WithTimeout(ctx, 30*time.Second)
+	defer cancel()
+
+	return c.QueryStruct(queryCtx, "POST", InternalEndpoint, api.NewURL().Path("hooks", string(types.PostRemove)), config, nil)
+}
+
+// RunNewMemberHook executes the OnNewMember hook with the given configuration on the cluster member targetted by this client.
+func RunNewMemberHook(ctx context.Context, c *Client, config types.HookNewMemberOptions) error {
+	queryCtx, cancel := context.WithTimeout(ctx, 30*time.Second)
+	defer cancel()
+
+	return c.QueryStruct(queryCtx, "POST", InternalEndpoint, api.NewURL().Path("hooks", string(types.OnNewMember)), config, nil)
+}

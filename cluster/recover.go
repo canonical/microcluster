e64bd53d00c2ab9c50f2331fac1a26aa96d8cced
Wesley Hershberger <wesley.hershberger@canonical.com>
2024-06-13 16:06:26 -0500

    cluster: Add local cluster Member
    For recovery
    
    Signed-off-by: Wesley Hershberger <wesley.hershberger@canonical.com>

---
+++ cluster/recover.go
@@ -0,0 +1,40 @@
+package cluster
+
+import (
+	"fmt"
+
+	"github.com/canonical/go-dqlite"
+	dqliteClient "github.com/canonical/go-dqlite/client"
+)
+
+// LocalMember is the information that can be derived locally about a cluster member
+// without access to the dqlite database.
+type LocalMember struct {
+	// dqlite.NodeInfo fields
+	DqliteID uint64 `json:"id" yaml:"id"`
+	Address  string `json:"address" yaml:"address"`
+	Role     string `json:"role" yaml:"role"`
+
+	Name string `json:"name" yaml:"name"`
+}
+
+// NodeInfo is used for interop with go-dqlite.
+func (m LocalMember) NodeInfo() (*dqlite.NodeInfo, error) {
+	var role dqliteClient.NodeRole
+	switch m.Role {
+	case "voter":
+		role = dqliteClient.Voter
+	case "stand-by":
+		role = dqliteClient.StandBy
+	case "spare":
+		role = dqliteClient.Spare
+	default:
+		return nil, fmt.Errorf("invalid dqlite role %q", m.Role)
+	}
+
+	return &dqlite.NodeInfo{
+		ID:      m.DqliteID,
+		Role:    role,
+		Address: m.Address,
+	}, nil
+}

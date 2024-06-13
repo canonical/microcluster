2e62e44585c5c9eac20e327428d680c0ad7108cb
Wesley Hershberger <wesley.hershberger@canonical.com>
2024-06-13 16:10:41 -0500

    example/test: Allow creating arbitrary-sized clusters
    Signed-off-by: Wesley Hershberger <wesley.hershberger@canonical.com>

--- example/test/main.sh
+++ example/test/main.sh
@@ -18,7 +18,7 @@ if [ -d "${test_dir}" ]; then
   rm -r "${test_dir}"
 fi
 
-members=("c1" "c2" "c3")
+members=("c1" "c2" "c3" "c4" "c5")
 
 for member in "${members[@]}"; do
   state_dir="${test_dir}/${member}"
@@ -30,20 +30,23 @@ done
 # Ensure two daemons cannot start in the same state dir
 ! microd --state-dir "${test_dir}/c1" "${cluster_flags[@]}"
 
-# Ensure only valid member names are used
+# Ensure only valid member names are used for bootstrap
 ! microctl --state-dir "${test_dir}/c1" init "c/1" 127.0.0.1:9001 --bootstrap
 
 microctl --state-dir "${test_dir}/c1" init "c1" 127.0.0.1:9001 --bootstrap
 
-# Ensure only valid member names are used
+# Ensure only valid member names are used for join
 token_node2=$(microctl --state-dir "${test_dir}/c1" tokens add "c/2")
-! microctl --state-dir "${test_dir}/c1" init "c/2" 127.0.0.1:9003 --token "${token_node2}"
+! microctl --state-dir "${test_dir}/c1" init "c/2" 127.0.0.1:9002 --token "${token_node2}"
 
-token_node2=$(microctl --state-dir "${test_dir}/c1" tokens add "c2")
-token_node3=$(microctl --state-dir "${test_dir}/c1" tokens add "c3")
+indx=2
+for member in "${members[@]:1}"; do
+  token=$(microctl --state-dir "${test_dir}/c1" tokens add "${member}")
 
-microctl --state-dir "${test_dir}/c2" init "c2" 127.0.0.1:9002 --token "${token_node2}"
-microctl --state-dir "${test_dir}/c3" init "c3" 127.0.0.1:9003 --token "${token_node3}"
+  microctl --state-dir "${test_dir}/${member}" init "${member}" "127.0.0.1:900${indx}" --token "${token}"
+
+  indx=$((indx + 1))
+done
 
 # Clean up
 if [ -n "${CLUSTER_INSPECT:-}" ]; then
@@ -51,6 +54,11 @@ if [ -n "${CLUSTER_INSPECT:-}" ]; then
   read -r
 fi
 
-kill %1
-kill %2
-kill %3
+for member in "${members[@]}"; do
+  microctl --state-dir "${test_dir}/${member}" shutdown
+done
+
+kill 0
+
+sleep 1
+

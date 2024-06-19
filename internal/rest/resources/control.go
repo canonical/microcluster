8d512dd2d3d71064943f1f3e9441029f9ac3b2a6
Max Asnaashari <max.asnaashari@canonical.com>
2024-06-19 22:49:50 +0000

    internal/rest/resources: Allow FQDN cluster names
    Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>

--- internal/rest/resources/control.go
+++ internal/rest/resources/control.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/canonical/lxd/lxd/response"
 	"github.com/canonical/lxd/lxd/util"
@@ -29,6 +30,24 @@ var controlCmd = rest.Endpoint{
 	Post: rest.EndpointAction{Handler: controlPost, AccessHandler: access.AllowAuthenticated},
 }
 
+// validateFQDN validates that the given name is a a valid fully qualified domain name.
+func validateFQDN(name string) error {
+	// Validate length
+	if len(name) < 1 || len(name) > 255 {
+		return fmt.Errorf("Name must be 1-255 characters long")
+	}
+
+	hostnames := strings.Split(name, ".")
+	for _, h := range hostnames {
+		err := validate.IsHostname(h)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
 func controlPost(state *state.State, r *http.Request) response.Response {
 	req := &internalTypes.Control{}
 	// Parse the request.
@@ -41,7 +60,7 @@ func controlPost(state *state.State, r *http.Request) response.Response {
 		return response.SmartError(fmt.Errorf("Invalid options - received join token and bootstrap flag"))
 	}
 
-	err = validate.IsHostname(req.Name)
+	err = validateFQDN(req.Name)
 	if err != nil {
 		return response.SmartError(fmt.Errorf("Invalid cluster member name %q: %w", req.Name, err))
 	}

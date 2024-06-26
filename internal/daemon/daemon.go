79dbf7b4fe5f4c1e8590fe8c32b10be47df06f00
Max Asnaashari <max.asnaashari@canonical.com>
2024-06-26 05:33:57 +0000

    internal/daemon: Attempt to re-use control.socket if present
    If the daemon crashed or stopped uncleanly due to an error, then the
    control socket file might still be present in the state directory. This
    means the daemon will enter a restart loop until the control socket is
    manually removed.
    
    Instead, we can just attempt to re-connect to the control socket. If
    it's already in use then that will error out the daemon anyway.
    
    Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>

--- internal/daemon/daemon.go
+++ internal/daemon/daemon.go
@@ -118,15 +118,6 @@ func (d *Daemon) Run(ctx context.Context, listenPort string, stateDir string, so
 		return fmt.Errorf("Failed to initialize directory structure: %w", err)
 	}
 
-	isAlreadyRunning, err := d.os.IsControlSocketPresent()
-	if err != nil {
-		return err
-	}
-
-	if isAlreadyRunning {
-		return fmt.Errorf("Control socket already present (%q); is another daemon already running?", d.os.ControlSocketPath())
-	}
-
 	d.extensionServers = extensionServers
 
 	err = d.init(listenPort, extensionsSchema, apiExtensions, hooks)

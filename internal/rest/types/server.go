aa5c6744bba66dfac5ca0c7ed6aa3c8ba2d96148
Gabriel Mougard <gabriel.mougard@canonical.com>
2024-07-08 10:04:58 +0200

    microcluster: Expose the API extensions to the status API
    In order to easily retrieve a MicroCluster service apiExtensions, we need to expose them to a public API
    
    Signed-off-by: Gabriel Mougard <gabriel.mougard@canonical.com>

--- internal/rest/types/server.go
+++ internal/rest/types/server.go
@@ -1,14 +1,16 @@
 package types
 
 import (
+	"github.com/canonical/microcluster/internal/extensions"
 	"github.com/canonical/microcluster/rest/types"
 )
 
 // Server represents server status information.
 type Server struct {
-	Name    string         `json:"name"    yaml:"name"`
-	Address types.AddrPort `json:"address" yaml:"address"`
-	Ready   bool           `json:"ready"   yaml:"ready"`
+	Name       string                `json:"name"    yaml:"name"`
+	Address    types.AddrPort        `json:"address" yaml:"address"`
+	Ready      bool                  `json:"ready"   yaml:"ready"`
+	Extensions extensions.Extensions `json:"extensions" yaml:"extensions"`
 }
 
 const (

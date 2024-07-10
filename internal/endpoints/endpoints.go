bd0eedcb5fce759e2da5115ed2c32f6b53a34047
Julian Pelizäus <julian.pelizaeus@canonical.com>
2024-07-10 19:15:07 +0200

    internal/endpoints: Use map type to track endpoints
    Signed-off-by: Julian Pelizäus <julian.pelizaeus@canonical.com>

--- internal/endpoints/endpoints.go
+++ internal/endpoints/endpoints.go
@@ -13,17 +13,12 @@ type Endpoints struct {
 	mu          sync.RWMutex
 	shutdownCtx context.Context // Parent context for shutting down cleanly.
 
-	listeners map[EndpointType]Endpoint // Map of supported listeners.
+	listeners map[string]Endpoint // Map of supported listeners.
 }
 
 // NewEndpoints aggregates the given endpoints so we can manage them from one source.
-func NewEndpoints(shutdownCtx context.Context, endpoints ...Endpoint) *Endpoints {
-	listeners := map[EndpointType]Endpoint{}
-	for _, endpoint := range endpoints {
-		listeners[endpoint.Type()] = endpoint
-	}
-
-	return &Endpoints{listeners: listeners, shutdownCtx: shutdownCtx}
+func NewEndpoints(shutdownCtx context.Context, endpoints map[string]Endpoint) *Endpoints {
+	return &Endpoints{listeners: endpoints, shutdownCtx: shutdownCtx}
 }
 
 // Up calls Serve on each of the configured listeners.
@@ -39,34 +34,33 @@ func (e *Endpoints) Up() error {
 	return nil
 }
 
-// UpdateTLS updates the TLS configuration of the network listeners.
-func (e *Endpoints) UpdateTLS(cert *shared.CertInfo) {
+// UpdateTLSByName updates the TLS configuration of the network listeners.
+// In case the cluster certificate gets updated reload the core.
+func (e *Endpoints) UpdateTLSByName(name string, cert *shared.CertInfo) {
 	e.mu.Lock()
 	defer e.mu.Unlock()
 
-	for _, l := range e.listeners {
-		n, ok := l.(*Network)
+	endpoint, ok := e.listeners[name]
+	if ok {
+		network, ok := endpoint.(*Network)
 		if ok {
-			n.UpdateTLS(cert)
+			network.UpdateTLS(cert)
 		}
 	}
 }
 
 // Add calls Serve on the additional set of listeners, and adds them to Endpoints.
-func (e *Endpoints) Add(endpoints ...Endpoint) error {
-	newListeners := map[EndpointType]Endpoint{}
-	for _, endpoint := range endpoints {
-		newListeners[endpoint.Type()] = endpoint
-	}
-
-	for k, v := range newListeners {
+func (e *Endpoints) Add(endpoints map[string]Endpoint) error {
+	for k, v := range endpoints {
 		e.listeners[k] = v
 	}
 
-	err := e.up(newListeners)
+	err := e.up(endpoints)
 	if err != nil {
-		// Attempt to call Down() in case something actually got brought up.
-		_ = e.Down()
+		// Attempt to call DownByName() in case something actually got brought up.
+		for name := range endpoints {
+			_ = e.DownByName(name)
+		}
 
 		return err
 	}
@@ -74,27 +68,27 @@ func (e *Endpoints) Add(endpoints ...Endpoint) error {
 	return nil
 }
 
-func (e *Endpoints) up(listeners map[EndpointType]Endpoint) error {
+func (e *Endpoints) up(listeners map[string]Endpoint) error {
 	e.mu.Lock()
 	defer e.mu.Unlock()
 
 	// Startup listeners.
-	for key := range listeners {
-		err := listeners[key].Listen()
+	for _, listener := range listeners {
+		listenerCopy := listener
+
+		err := listenerCopy.Listen()
 		if err != nil {
 			return err
 		}
 
-		listener := listeners[key]
-
 		go func() {
 			select {
 			case <-e.shutdownCtx.Done():
-				logger.Infof("Received shutdown signal - aborting endpoint startup for %s", listener.Type().String())
+				logger.Infof("Received shutdown signal - aborting endpoint startup for %s", listenerCopy.Type().String())
 				return
 
 			default:
-				listener.Serve()
+				listenerCopy.Serve()
 			}
 		}()
 	}
@@ -107,37 +101,42 @@ func (e *Endpoints) Down(types ...EndpointType) error {
 	e.mu.Lock()
 	defer e.mu.Unlock()
 
-	for _, listener := range e.listeners {
+	for name, endpoint := range e.listeners {
 		remove := false
-		for _, endpoint := range types {
-			if listener.Type() == endpoint {
+		for _, endpointType := range types {
+			if endpoint.Type() == endpointType {
 				remove = true
 			}
 		}
 
 		if types == nil || remove {
-			err := listener.Close()
+			err := endpoint.Close()
 			if err != nil {
 				return err
 			}
+
+			// Delete the stopped endpoint from the slice.
+			delete(e.listeners, name)
 		}
 	}
 
 	return nil
 }
 
-// DownBy closes the configured listeners based on the decisionFunc.
-// If it returns true the respective listener gets closed.
-func (e *Endpoints) DownBy(decisionFunc func(endpoint Endpoint) bool) error {
+// DownByName closes the configured listeners based on its name.
+func (e *Endpoints) DownByName(name string) error {
 	e.mu.Lock()
 	defer e.mu.Unlock()
 
-	for _, listener := range e.listeners {
-		if decisionFunc(listener) {
-			err := listener.Close()
+	for endpointName, endpoint := range e.listeners {
+		if endpointName == name {
+			err := endpoint.Close()
 			if err != nil {
 				return err
 			}
+
+			// Delete the stopped endpoint from the slice.
+			delete(e.listeners, name)
 		}
 	}
 
@@ -145,14 +144,14 @@ func (e *Endpoints) DownBy(decisionFunc func(endpoint Endpoint) bool) error {
 }
 
 // List returns a list of already added endpoints.
-func (e *Endpoints) List(types ...EndpointType) []Endpoint {
+func (e *Endpoints) List(types ...EndpointType) map[string]Endpoint {
 	e.mu.Lock()
 	defer e.mu.Unlock()
 
-	var endpoints = make([]Endpoint, 0)
-	for _, listener := range e.listeners {
-		if shared.ValueInSlice(listener.Type(), types) {
-			endpoints = append(endpoints, listener)
+	var endpoints = make(map[string]Endpoint, 0)
+	for name, endpoint := range e.listeners {
+		if shared.ValueInSlice(endpoint.Type(), types) {
+			endpoints[name] = endpoint
 		}
 	}
 

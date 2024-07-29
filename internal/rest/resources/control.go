1974a76f0bac0fc930c3500fe1f22b2c4b0a058b
Julian Pelizäus <julian.pelizaeus@canonical.com>
2024-07-29 10:45:04 +0200

    internal/rest/resources: Move validateFQDN into dedicated utils package
    Signed-off-by: Julian Pelizäus <julian.pelizaeus@canonical.com>

--- internal/rest/resources/control.go
+++ internal/rest/resources/control.go
@@ -34,24 +34,6 @@ var controlCmd = rest.Endpoint{
 	Post: rest.EndpointAction{Handler: controlPost, AccessHandler: access.AllowAuthenticated},
 }
 
-// validateFQDN validates that the given name is a a valid fully qualified domain name.
-func validateFQDN(name string) error {
-	// Validate length
-	if len(name) < 1 || len(name) > 255 {
-		return fmt.Errorf("Name must be 1-255 characters long")
-	}
-
-	hostnames := strings.Split(name, ".")
-	for _, h := range hostnames {
-		err := validate.IsHostname(h)
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
 func controlPost(state state.State, r *http.Request) response.Response {
 	status := state.Database().Status()
 	if status != db.StatusNotReady {

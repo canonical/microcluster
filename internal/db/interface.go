23bc99551f92226c9b6e0174188a40ab39f30993
Max Asnaashari <max.asnaashari@canonical.com>
2024-07-30 19:47:39 +0000

    internal/db: Add DB interface
    Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>

---
+++ internal/db/interface.go
@@ -0,0 +1,33 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+
+	dqliteClient "github.com/canonical/go-dqlite/client"
+
+	"github.com/canonical/microcluster/internal/extensions"
+)
+
+// DB exposes the internal database for use with external projects.
+type DB interface {
+	// Transaction handles performing a transaction on the dqlite database.
+	Transaction(outerCtx context.Context, f func(context.Context, *sql.Tx) error) error
+
+	// Leader returns a client connected to the leader of the dqlite cluster.
+	Leader(ctx context.Context) (*dqliteClient.Client, error)
+
+	// Cluster returns information about dqlite cluster members.
+	Cluster(ctx context.Context, client *dqliteClient.Client) ([]dqliteClient.NodeInfo, error)
+
+	// Status returns the current status of the database.
+	Status() Status
+
+	// IsOpen returns nil  only if the DB has been opened and the schema loaded.
+	// Otherwise, it returns an error describing why the database is offline.
+	// The returned error may have the http status 503, indicating that the database is in a valid but unavailable state.
+	IsOpen(ctx context.Context) error
+
+	// SchemaVersion returns the current internal and external schema version, as well as all API extensions in memory.
+	SchemaVersion() (versionInternal uint64, versionExternal uint64, apiExtensions extensions.Extensions)
+}

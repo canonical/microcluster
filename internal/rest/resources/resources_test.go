b317b2bb8b259191c585669b68835ab79f99293c
Wesley Hershberger <wesley.hershberger@canonical.com>
2024-08-09 15:59:24 -0500

    internal/core/resources: Unittests for ValidateResources
    Signed-off-by: Wesley Hershberger <wesley.hershberger@canonical.com>

---
+++ internal/rest/resources/resources_test.go
@@ -0,0 +1,101 @@
+package resources
+
+import (
+	"testing"
+
+	"github.com/canonical/microcluster/v3/rest"
+)
+
+var validServers = map[string]rest.Server{
+	"coreConsumer": {
+		CoreAPI: true,
+		Resources: []rest.Resources{
+			{
+				PathPrefix: "core_consumer",
+			},
+		},
+	},
+}
+
+func TestValidateEndpointsValidServers(t *testing.T) {
+	err := ValidateEndpoints(validServers, "localhost:8000")
+	if err != nil {
+		t.Errorf("Valid server failed validation: %s", err)
+	}
+}
+
+var invalidServers = map[string]rest.Server{
+	"emptyResources": {
+		CoreAPI: true,
+	},
+	"duplicate": {
+		Resources: []rest.Resources{
+			{
+				PathPrefix: "dup",
+				Endpoints: []rest.Endpoint{
+					{
+						Path: "duplicate",
+					},
+				},
+			},
+			{
+				PathPrefix: "dup",
+				Endpoints: []rest.Endpoint{
+					{
+						Path: "duplicate",
+					},
+				},
+			},
+		},
+	},
+	"overlapCore": {
+		CoreAPI: true,
+		Resources: []rest.Resources{
+			{
+				PathPrefix: "core",
+				Endpoints: []rest.Endpoint{
+					{
+						Path: "hello",
+					},
+				},
+			},
+		},
+	},
+	"overlapCoreMultipart": {
+		CoreAPI: true,
+		Resources: []rest.Resources{
+			{
+				PathPrefix: "core/subpoint",
+				Endpoints: []rest.Endpoint{
+					{
+						Path: "hello",
+					},
+				},
+			},
+		},
+	},
+	"overlapCoreEndpoint": {
+		CoreAPI: true,
+		Resources: []rest.Resources{
+			{
+				Endpoints: []rest.Endpoint{
+					{
+						Path: "core/subpoint",
+					},
+				},
+			},
+		},
+	},
+}
+
+func TestValidateEndpointsInvalidServers(t *testing.T) {
+	for serverName, server := range invalidServers {
+		servers := map[string]rest.Server{
+			serverName: server,
+		}
+		err := ValidateEndpoints(servers, "localhost:8000")
+		if err == nil {
+			t.Errorf("Invalid server %q passed validation", serverName)
+		}
+	}
+}

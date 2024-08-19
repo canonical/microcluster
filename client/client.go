8959957e226e10cb5f76f359dc2c6e75f2859d91
Julian Pelizäus <julian.pelizaeus@canonical.com>
2024-08-19 09:52:46 +0200

    client: Add Websocket client function for external use
    Signed-off-by: Julian Pelizäus <julian.pelizaeus@canonical.com>

--- client/client.go
+++ client/client.go
@@ -6,6 +6,7 @@ import (
 
 	clusterRequest "github.com/canonical/lxd/lxd/cluster/request"
 	"github.com/canonical/lxd/shared/api"
+	"github.com/gorilla/websocket"
 
 	"github.com/canonical/microcluster/v3/internal/rest/client"
 	"github.com/canonical/microcluster/v3/rest/types"
@@ -28,6 +29,15 @@ func (c *Client) Query(ctx context.Context, method string, prefix types.Endpoint
 	defer cancel()
 
 	return c.QueryStruct(queryCtx, method, prefix, path, in, &out)
+}
+
+// Websocket is a helper for upgrading a request to websocket on any endpoints defined external to microcluster.
+// This function should be used for all client methods defined externally from microcluster.
+func (c *Client) Websocket(ctx context.Context, prefix types.EndpointPrefix, path *api.URL) (*websocket.Conn, error) {
+	websocketCtx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	return c.RawWebsocket(websocketCtx, prefix, path)
 }
 
 // UseTarget returns a new client with the query "?target=name" set.

3393440a4b437bd538e86cfacedeabd79d38815a
Julian Pelizäus <julian.pelizaeus@canonical.com>
2024-09-06 13:23:59 +0200

    client: Add QueryRaw
    Allow sending a query and getting the raw response.
    Externals can use the response.ParseResponse func to both access the raw
    response but still parse it as usual.
    
    Signed-off-by: Julian Pelizäus <julian.pelizaeus@canonical.com>
    (cherry picked from commit 057ddc2a0ad10952df1890bb3408a61d50e4f0ae)

--- client/client.go
+++ client/client.go
@@ -31,6 +31,15 @@ func (c *Client) Query(ctx context.Context, method string, prefix types.Endpoint
 	return c.QueryStruct(queryCtx, method, prefix, path, in, &out)
 }
 
+// QueryRaw is a helper for initiating a request on any endpoints defined external to microcluster.
+// Unlike Query it returns the raw HTTP response.
+func (c *Client) QueryRaw(ctx context.Context, method string, prefix types.EndpointPrefix, path *api.URL, in any) (*http.Response, error) {
+	queryCtx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	return c.QueryStructRaw(queryCtx, method, prefix, path, in)
+}
+
 // Websocket is a helper for upgrading a request to websocket on any endpoints defined external to microcluster.
 // This function should be used for all client methods defined externally from microcluster.
 func (c *Client) Websocket(ctx context.Context, prefix types.EndpointPrefix, path *api.URL) (*websocket.Conn, error) {

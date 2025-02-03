08169c3d5027f7cee569b4d2bde1c68b551f67ef
Julian Pelizäus <julian.pelizaeus@canonical.com>
2025-02-03 15:24:19 +0100

    test/lint: Add no-online-assing-and-test from LXD
    Signed-off-by: Julian Pelizäus <julian.pelizaeus@canonical.com>
    (cherry picked from commit bff241b7c3aefe6375f8dd70658e3e6fea849b53)

---
+++ test/lint/no-oneline-assign-and-test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -eu
+
+echo "Checking for oneline assign & test..."
+
+# Recursively grep go files for if statements that contain assignments.
+! git grep --untracked -P -n '^\s+if.*:=.*;.*{\s*$' -- '*.go'

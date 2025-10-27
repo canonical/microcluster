e170854cdb88f9aeb4de442b5152cedc2bcb15d8
Julian Pelizäus <julian.pelizaeus@canonical.com>
2025-10-27 10:07:50 +0100

    internal/daemon: Accept our own logger for the log filter and use it
    Signed-off-by: Julian Pelizäus <julian.pelizaeus@canonical.com>

--- internal/daemon/logfilter.go
+++ internal/daemon/logfilter.go
@@ -2,29 +2,34 @@ package daemon
 
 import (
 	"bytes"
+	"log/slog"
 	"regexp"
 	"strings"
-
-	"github.com/canonical/lxd/shared/logger"
 
 	"github.com/canonical/microcluster/v3/rest/types"
 )
 
 // logFilter represents a filter for log messages caused by known addresses.
 type logFilter struct {
+	logger         *slog.Logger
 	knownAddresses func() map[string]types.AddrPort
 }
 
 // newLogFilter returns a new instances of the log filter.
 // It expects a slice of known addresses for which log messages will get filtered.
-func newLogFilter(knownAddresses func() map[string]types.AddrPort) *logFilter {
+func newLogFilter(logger *slog.Logger, knownAddresses func() map[string]types.AddrPort) *logFilter {
 	return &logFilter{
+		logger:         logger,
 		knownAddresses: knownAddresses,
 	}
 }
 
 // unwantedLogRegex represents log texts whose presence will cause log messages to be filtered out.
 var unwantedLogRegex = regexp.MustCompile(`^http: TLS handshake error from (.*):[0-9]+: EOF$`)
+
+func (l logFilter) log() *slog.Logger {
+	return l.logger
+}
 
 // Write filters the given log message p and checks whether or not it contains any
 // of the unwanted log messages.
@@ -36,7 +41,7 @@ func (l logFilter) Write(p []byte) (int, error) {
 		return 0, nil
 	}
 
-	logger.Info(strippedLog)
+	l.log().Info(strippedLog)
 	return len(p), nil
 }
 
@@ -63,7 +68,7 @@ func (l logFilter) stripLog(p []byte) string {
 	if sourceIP != "" {
 		for _, knownAddress := range l.knownAddresses() {
 			if knownAddress.Addr().String() == sourceIP {
-				logger.Debug("Filtered out unwanted log text", logger.Ctx{"text": logStr})
+				l.log().Debug("Filtered out unwanted log text", slog.String("text", logStr))
 				return ""
 			}
 		}

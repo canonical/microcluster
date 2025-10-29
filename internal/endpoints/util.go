cb51eb12ab776b46deffdb45d2e75002a7237828
Julian Pelizäus <julian.pelizaeus@canonical.com>
2025-10-29 15:19:03 +0100

    internal/endpoints: Copy canonicalNetworkAddress func from LXD
    Signed-off-by: Julian Pelizäus <julian.pelizaeus@canonical.com>

--- internal/endpoints/util.go
+++ internal/endpoints/util.go
@@ -7,6 +7,7 @@ import (
 	"log/slog"
 	"net"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/canonical/microcluster/v3/internal/log"
@@ -47,4 +48,28 @@ func shutdownServer(ctx context.Context, server *http.Server, timeout time.Durat
 	}
 
 	return nil
+}
+
+// canonicalNetworkAddress parses the given network address and returns a string of the form "host:port",
+// possibly filling it with the default port if it's missing. It will also wrap a bare IPv6 address with square
+// brackets if needed.
+func canonicalNetworkAddress(address string, defaultPort int64) string {
+	host, port, err := net.SplitHostPort(address)
+	if err != nil {
+		ip := net.ParseIP(address)
+		if ip != nil {
+			// If the input address is a bare IP address, then convert it to a proper listen address
+			// using the canonical IP with default port and wrap IPv6 addresses in square brackets.
+			address = net.JoinHostPort(ip.String(), strconv.FormatInt(defaultPort, 10))
+		} else {
+			// Otherwise assume this is either a host name or a partial address (e.g `[::]`) without
+			// a port number, so append the default port.
+			address = address + ":" + strconv.FormatInt(defaultPort, 10)
+		}
+	} else if port == "" && address[len(address)-1] == ':' {
+		// An address that ends with a trailing colon will be parsed as having an empty port.
+		address = net.JoinHostPort(host, strconv.FormatInt(defaultPort, 10))
+	}
+
+	return address
 }

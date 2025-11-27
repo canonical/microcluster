d60167c10d8c0562262189425157f2289b2bb222
Julian Pelizäus <julian.pelizaeus@canonical.com>
2025-11-27 10:34:25 +0100

    example/client: Add new ExtendedWebsocketCmd client func
    Signed-off-by: Julian Pelizäus <julian.pelizaeus@canonical.com>
    (cherry picked from commit 6cf8489996d01a38fd4ec406c81adbf63e28c31a)

--- example/client/client.go
+++ example/client/client.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/canonical/lxd/shared/api"
+	"github.com/gorilla/websocket"
 
 	"github.com/canonical/microcluster/v2/client"
 	"github.com/canonical/microcluster/v2/example/api/types"
@@ -27,4 +28,33 @@ func ExtendedSimpleCmd(ctx context.Context, c *client.Client, data *types.Extend
 	}
 
 	return outStr, nil
+}
+
+// ExtendedWebsocketCmd is a client function that sets a context timeout and sends a GET to /1.0/extended/websocket using the given
+// client. This function is expected to be called from an api endpoint handler, which gives us access to the
+// daemon state, from which we can create a client.
+func ExtendedWebsocketCmd(ctx context.Context, c *client.Client) error {
+	queryCtx, cancel := context.WithTimeout(ctx, time.Second*30)
+	defer cancel()
+
+	conn, err := c.Websocket(queryCtx, types.ExtendedPathPrefix, api.NewURL().Path("extended", "websocket"))
+	if err != nil {
+		return fmt.Errorf("Failed to create websocket connection: %w", err)
+	}
+
+	defer conn.Close()
+
+	for {
+		_, message, err := conn.ReadMessage()
+		if err != nil {
+			// If the server closes the connection, exit gracefully.
+			if websocket.IsCloseError(err, websocket.CloseAbnormalClosure) {
+				return nil
+			}
+
+			return fmt.Errorf("Failed to read message from websocket: %w", err)
+		}
+
+		fmt.Println(string(message))
+	}
 }

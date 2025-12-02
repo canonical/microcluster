0e3650cf03ebbfbc8b4741e524e9e96c17e9e0f5
Julian Pelizäus <julian.pelizaeus@canonical.com>
2025-12-02 14:12:58 +0100

    microcluster/rest: Drop top level rest package
    Signed-off-by: Julian Pelizäus <julian.pelizaeus@canonical.com>

--- microcluster/rest/rest.go
+++ microcluster/rest/rest.go
@@ -4,8 +4,8 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/canonical/microcluster/v3/rest/response"
-	"github.com/canonical/microcluster/v3/rest/types"
+	"github.com/canonical/microcluster/v3/microcluster/rest/response"
+	"github.com/canonical/microcluster/v3/microcluster/types"
 	"github.com/canonical/microcluster/v3/state"
 )
 

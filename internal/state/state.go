e30743807d3ed88e763974279e77a0dcb6121a06
MJ Ponsonby <mj.ponsonby@canonical.com>
2026-01-21 11:13:25 +0000

    internal/state: Update error message for SelectRandom().
    In a single node cluster, Cluster() will return an empty list. On doing
    so this error message makes it sound like there are no members in the
    cluster where there could in fact be only 1 (the local node), so this
    commit updates the error message accordingly.
    
    Signed-off-by: MJ Ponsonby <mj.ponsonby@canonical.com>

--- internal/state/state.go
+++ internal/state/state.go
@@ -261,7 +261,7 @@ func (s *InternalState) RandomMember(isNotification bool) (types.Client, error) 
 	switch clusterClientNum {
 	case 0:
 		// Returns an error if the cluster is uninitialized (not bootstrapped, not joined).
-		return nil, fmt.Errorf("Cluster is uninitialized or has no members")
+		return nil, fmt.Errorf("Cluster is uninitialized or has no other members")
 	case 1:
 		// Returns the only available client if cluster size is 1.
 		return clusterClients[0], nil

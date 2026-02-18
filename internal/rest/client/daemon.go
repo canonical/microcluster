764ab0413bde7992932101ef5d1ec7776eb3aac5
Kadin Sayani <kadin.sayani@canonical.com>
2026-02-18 08:55:03 -0800

    internal/rest/client: Add [Get|Update|Patch]DaemonConfig
    Signed-off-by: Kadin Sayani <kadin.sayani@canonical.com>
    (cherry picked from commit af2e04d3cd955599b165cc01d5211a60370c2b64)

--- internal/rest/client/daemon.go
+++ internal/rest/client/daemon.go
@@ -9,6 +9,39 @@ import (
 	"github.com/canonical/microcluster/v3/microcluster/types"
 )
 
+// GetDaemonConfig retrieves local daemon configuration.
+func GetDaemonConfig(ctx context.Context, c types.Client) (*types.DaemonConfig, error) {
+	queryCtx, cancel := context.WithTimeout(ctx, 30*time.Second)
+	defer cancel()
+
+	endpoint := api.NewURL().Path("daemon", "config")
+	config := types.DaemonConfig{}
+	err := c.Query(queryCtx, "GET", types.PublicEndpoint, &endpoint.URL, nil, &config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
+
+// UpdateDaemonConfig updates local daemon configuration.
+func UpdateDaemonConfig(ctx context.Context, c types.Client, config types.DaemonConfig) error {
+	queryCtx, cancel := context.WithTimeout(ctx, 30*time.Second)
+	defer cancel()
+
+	endpoint := api.NewURL().Path("daemon", "config")
+	return c.Query(queryCtx, "PUT", types.PublicEndpoint, &endpoint.URL, config, nil)
+}
+
+// PatchDaemonConfig partially updates local daemon configuration.
+func PatchDaemonConfig(ctx context.Context, c types.Client, config types.DaemonConfigPatch) error {
+	queryCtx, cancel := context.WithTimeout(ctx, 30*time.Second)
+	defer cancel()
+
+	endpoint := api.NewURL().Path("daemon", "config")
+	return c.Query(queryCtx, "PATCH", types.PublicEndpoint, &endpoint.URL, config, nil)
+}
+
 // UpdateServers updates the additional servers config.
 func UpdateServers(ctx context.Context, c types.Client, config map[string]types.ServerConfig) error {
 	queryCtx, cancel := context.WithTimeout(ctx, 30*time.Second)

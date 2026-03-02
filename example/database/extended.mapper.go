c8ced88f06cb7e754ded61b063fb9c4717a53964
Julian Pelizäus <julian.pelizaeus@canonical.com>
2026-03-02 13:43:29 +0100

    example/database: Use Microcluster v4
    Signed-off-by: Julian Pelizäus <julian.pelizaeus@canonical.com>

--- example/database/extended.mapper.go
+++ example/database/extended.mapper.go
@@ -10,7 +10,7 @@ import (
 
 	"github.com/canonical/lxd/shared/api"
 
-	"github.com/canonical/microcluster/v3/microcluster/db"
+	"github.com/canonical/microcluster/v4/microcluster/db"
 )
 
 var _ = api.ServerEnvironment{}
